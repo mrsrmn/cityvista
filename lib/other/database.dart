489ec96a61f466a5a3a4b8d1d22906994f996944
Emir <emirsurmen@gmail.com>
2023-09-03 17:45:16 +0300

    add profile class

--- lib/other/database.dart
+++ lib/other/database.dart
@@ -1,4 +1,7 @@
 import 'package:cityvista/other/models/city_place.dart';
+import 'package:cityvista/other/utils.dart';
+import 'package:cityvista/other/models/profile.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Database {
@@ -7,13 +10,23 @@ class Database {
   Future<void> addPlace(CityPlace place) async {
     Map<String, dynamic> placeMap = place.toJson();
 
-    try {
-      await firestore.collection("places").doc(place.id).set(placeMap);
-      await firestore.collection("users").doc(place.authorUid).update({
-        "places": FieldValue.arrayUnion([placeMap])
-      });
-    } catch (e) {
-      throw Exception(e.toString());
+    bool isProfanity = await Utils.checkProfanity("${place.name} ${place.description}");
+
+    if (isProfanity) {
+      throw Exception("Please remove any word from your name/description that can be offensive!");
     }
+
+    await firestore.collection("places").doc(place.id).set(placeMap);
+    await firestore.collection("users").doc(place.authorUid).update({
+      "places": FieldValue.arrayUnion([placeMap])
+    });
+  }
+
+  Future<Profile> getProfile(String uid) async {
+    Map<String, dynamic> data = (await firestore.collection(
+      "users"
+    ).doc(uid).get()).data()!;
+
+    return Profile.fromJson(data);
   }
 }
---
+++ lib/other/models/profile.dart
@@ -0,0 +1,70 @@
+import 'package:cityvista/other/models/city_place.dart';
+import 'package:cityvista/other/models/city_review.dart';
+
+class Profile {
+  final String phone;
+  final String displayName;
+  final List<CityPlace> favorites;
+  final List<CityReview> reviews;
+  final List<CityPlace> places;
+
+  Profile({
+    required this.phone,
+    required this.displayName,
+    required this.favorites,
+    required this.reviews,
+    required this.places
+  });
+
+  Map<String, dynamic> toJson() {
+    List<Map> mappedReviews = [];
+    List<Map> mappedFavorites = [];
+    List<Map> mappedPlaces = [];
+
+    for (CityReview review in reviews) {
+      mappedReviews.add(review.toJson());
+    }
+
+    for (CityPlace place in places) {
+      mappedPlaces.add(place.toJson());
+    }
+
+    for (CityPlace place in favorites) {
+      mappedFavorites.add(place.toJson());
+    }
+
+    return {
+      "phone": phone,
+      "displayName": displayName,
+      "favorites": mappedFavorites,
+      "reviews": mappedReviews,
+      "places": mappedPlaces
+    };
+  }
+
+  factory Profile.fromJson(Map<String, dynamic> data) {
+    List<CityReview> reviews = [];
+    List<CityPlace> places = [];
+    List<CityPlace> favorites = [];
+
+    for (var review in data["reviews"]) {
+      reviews.add(CityReview.fromJson(review));
+    }
+
+    for (var place in data["places"]) {
+      places.add(CityPlace.fromJson(place));
+    }
+
+    for (var place in data["favorites"]) {
+      favorites.add(CityPlace.fromJson(place));
+    }
+
+    return Profile(
+      phone: data["phone"],
+      displayName: data["displayName"],
+      favorites: favorites,
+      reviews: reviews,
+      places: places
+    );
+  }
+}
